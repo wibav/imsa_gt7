892888a495d3e0f61a6011599b67fa15bbf181f6
Cristian Da Silva <cdasilva@nexo-it.cl>
2026-03-06 17:34:05 +0000

    feat: add new equipment page and update navbar
    - Added a new "Equipamiento" page with recommended hardware for Gran Turismo 7.
    - Updated the Navbar component to include a link to the new "Equipamiento" page.
    - Modified the Next.js configuration to allow images from 'ws-eu.amazon-adsystem.com'.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-    domains: ['firebasestorage.googleapis.com', 'via.placeholder.com']
+    domains: ['firebasestorage.googleapis.com', 'via.placeholder.com', 'ws-eu.amazon-adsystem.com']
   },
   distDir: 'out',
   allowedDevOrigins: ['192.168.1.200']
